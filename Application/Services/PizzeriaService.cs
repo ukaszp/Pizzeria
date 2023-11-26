ab797cd364043f0b22f1fd8a2ac1812e21b70e6f
Łukasz Piróg <s9763lp@ms.wwsi.edu.pl>
2023-11-26 21:46:22 +0100

    d

---
+++ Application/Services/PizzeriaService.cs
@@ -0,0 +1,21 @@
+﻿using AccountApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Service.Services
+{
+    internal class PizzeriaService
+    {
+        private readonly IUserService userService;
+        private readonly IRoleService roleService;
+
+        public PizzeriaService(IUserService userService, IRoleService roleService)
+        {
+            this.userService = userService;
+            this.roleService = roleService;
+        }
+    }
+}
